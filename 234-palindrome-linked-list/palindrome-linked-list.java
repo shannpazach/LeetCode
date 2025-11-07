a7e18d1e7053fbe65a8babd7a450dcd24968409e
shannpazach <shannpazach@gmail.com>
2025-11-07 15:26:52 -0800

    Time: 11 ms (17.56%) | Memory: 91.9 MB (8.74%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,56 +10,27 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
+        Stack<Integer> stack = new Stack<>();
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            stack.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        ListNode copy = copyList(head);
-        ListNode reverse = reverseList(copy);
-        return checkSame(head, reverse);
-    }
-
-    private ListNode copyList(ListNode head){
-            if(head == null){
-                return null;
-            }
-            ListNode dummy = new ListNode(0);
-            ListNode tail = dummy;
-            ListNode curr = head;
-
-            while(curr!=null){
-                tail.next = new ListNode(curr.val);
-                tail = tail.next;
-                curr = curr.next;
-            }
-            return dummy.next;
+        if(fast!=null){
+            slow = slow.next;
         }
 
-    private ListNode reverseList(ListNode head){
-        if(head == null){
-            return null;
-        }
-
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while(curr!=null){
-            ListNode newNode = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = newNode;
-        }
-
-        return prev;
-    }
-
-    private boolean checkSame(ListNode l1, ListNode l2){
-        while(l1!=null && l2!=null){
-            if(l1.val!=l2.val){
+        while(!stack.isEmpty()){
+            int top = stack.pop();
+            if(top!=slow.val){
                 return false;
             }
-            l1 = l1.next;
-            l2 = l2.next;
+            slow = slow.next;
         }
         return true;
     }
