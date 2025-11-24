6e8c502c34857ea92062aefbf1524fdcbce8468b
shannpazach <shannpazach@gmail.com>
2025-11-24 12:38:55 -0800

    Time: 0 ms (100.00%) | Memory: 42.6 MB (25.76%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+
+        // String s1 = s+s;
+        // if(s1.contains(goal)){
+        //     return true;
+        // }
+        // return false;
+
+        // using indexOf()
+        String s1 = s+s;
+        if(s1.indexOf(goal)!=-1){
+            return true;
+        }
+        return false;
+    }
+}
