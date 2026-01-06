baa5ffb6fa9b5de7ad3e6e94f00a6ed79305d0a8
shannpazach <shannpazach@gmail.com>
2026-01-06 13:47:22 -0800

    Time: 1 ms (96.28%) | Memory: 42.8 MB (23.18%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,5 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.floor(Math.sqrt(x));
+        if(x<2){
+            return x;
+        }
+
+        int left = 1, right = x/2;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            long sq = (long)mid*mid;
+            if(sq == x){
+                return mid;
+            }
+            else if(sq < x){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return right;
     }
 }
